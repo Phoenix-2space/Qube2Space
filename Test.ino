9bd4da91dad6bff37d6f916167f5ecf1dd6f4e8f
StealthyCody <76008318+StealthyCody@users.noreply.github.com>
2022-09-11 16:11:17 +0300

    Update Test.ino

--- Test.ino
+++ Test.ino
@@ -1,38 +1,72 @@
 #include <Wire.h>                           // Folosita pentru comunicarea prin protocolul I2C
 #include <MS5611.h>                         // Folosita pentru a interactiona cu senzorul de presiune si temperatura MS5411 / GY63
+#include <SD.h>
+#include <SPI.h>
 
+File myFile;
+int pinCS = 8;                              // D8 
 MS5611 ms5611;
-double presiuneSol;
+float presiuneSol;
 
-void debug_ms5611(double t, long p, float a, float A);
+void debug_ms5611(float t, long p, float a, float A);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(pinCS, OUTPUT);
+  
+  if(SD.begin()){
+    Serial.println("Card SD OK");
+  }
+  else{
+    Serial.println("Problema la conectarea cardului SD");
+  }
+  
   Serial.println("Initializare senzor MS5611 / GY63");
-  while(!ms5611){
+  while(!ms5611.begin()){
     Serial.println("Problema la conectarea senzorului MS5611 / GY63");
     delay(500);
   }
-  presiuneSol = ms5611.readPressure();                                        // Presiunea initiala (de la sol) este folosita pentru a calcula altitudinea
+  presiuneSol = ms5611.getPressure();                                        // Presiunea initiala (de la sol) este folosita pentru a calcula altitudinea
+
+  myFile = SD.open("date.csv", FILE_WRITE);                                   // Initializare cap de tabel 
+  if(myFile){
+    myFile.println("Altitudine,Presiune,Temperatura");
+    myFile.close();
+  }
+  else{
+    Serial.println("Eroare la deschiderea fisierului date.csv");
+  }
 }
 
 void loop() {
-  double temperatura = ms5611.readTemperature();
-  long presiune = ms5611.readPressure();
-  float altitudineAbsoluta = ms5611.getAltitude(presiune);                    // Altitudine masurata de la nivelul marii
-  float altitudine = ms5611.getAltitude(presiune, presiuneSol);               // Altitudinea reala
+  float temperatura = ms5611.getTemperature();                 // Masurata in grade celsius
+  float presiune = ms5611.getPressure();                        // Masurata in milibari 1 mBa = 100 Pa
+  float altitudine;
+
+  myFile = SD.open("date.csv", FILE_WRITE);
+  if(myFile){
+    myFile.print(altitudine);
+    myFile.print(",");
+    myFile.print(presiune);
+    myFile.print(",");
+    myFile.println(temperatura);
+    myFile.close();
+  }
+  else{
+    Serial.println("Eroare la deschiderea fisierului date.csv");
+  }
   
   //debug_ms5611(temperatura, presiune, altitudineAbsoluta, altitudine);
   //delay(500);
 }
 
-void debug_ms5611(double t, long p, float a, float A){
+void debug_ms5611(float t, long p, float a, float A){
   Serial.println("-------------------");
   Serial.print("Temperatura: ");
   Serial.println(t);
   Serial.print("Presiune: ");
   Serial.println(p);
-  Serial.print("Altitudinea relativa marii: ")
+  Serial.print("Altitudinea relativa marii: ");
   Serial.println(a);
   Serial.print("Altitudinea reala: ");
   Serial.println(A);
