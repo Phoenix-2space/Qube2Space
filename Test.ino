da1ddb447c503ded5a20e34a3a82349d44ea45fa
Cookietza <victor.s.florescu@gmail.com>
2022-11-04 13:40:17 +0200

    Test.ino now works for the rigfht hardware
    added all libraries

--- Test.ino
+++ Test.ino
@@ -1,79 +1,118 @@
-#include <Wire.h>                           // Folosita pentru comunicarea prin protocolul I2C
-#include <MS5611.h>                         // Folosita pentru a interactiona cu senzorul de presiune si temperatura MS5411 / GY63
-#include <MQ135.h>                          // Folosita pentru a interactiona cu senzorul de gaze MQ135
-#include <SD.h>
 #include <SPI.h>
+#include <SD.h>
+#include "MS5611.h"
 
-File myFile;
-int pinCS = 8;                              // D8 
-MS5611 ms5611;
-MQ135 mq135_sensor(A0);                     // Senzorul de gaze necesita calibrare si un mic setup code ce il vom adauga la momentul constructiei
-double presiuneSol;
+MS5611 MS5611(0x77);
 
-void debug_ms5611(double t, double p, double a, double A);
+#ifndef LED_BUILTIN
+#define LED_BUILTIN    13
+#endif
+
+const int chipSelect = 8;
+
+uint32_t start, stop;
+
+int smokeA0 = A0;
+
+double altitudine_sol = 0.0;
+
+File dataFile;
+
+double func(double current_pressure, double sea_level_pressure=1013.25){
+    double a = current_pressure/sea_level_pressure;
+    double b = 1/5.255;
+    double exp = 44330 * (1 - pow(a, b));
+    return exp;
+}
+
+double altitudine(double presiune_senzor){
+    return func(presiune_senzor) - altitudine_sol;
+}
 
 void setup() {
+  // serial
   Serial.begin(9600);
-  pinMode(pinCS, OUTPUT);
-  
-  if(SD.begin()){
-    Serial.println("Card SD OK");
-  }
-  else{
-    Serial.println("Problema la conectarea cardului SD");
-  }
-  
-  Serial.println("Initializare senzor MS5611 / GY63");
-  while(!ms5611.begin()){
-    Serial.println("Problema la conectarea senzorului MS5611 / GY63");
-    delay(500);
-  }
-  presiuneSol = ms5611.getPressure();                                        // Presiunea initiala (de la sol) este folosita pentru a calcula altitudinea
+  while (!Serial);
+  // pinmode
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  // sd begin
+  Serial.print("Initializing SD card...");
 
-  myFile = SD.open("date.csv", FILE_WRITE);                                   // Initializare cap de tabel 
-  if(myFile){
-    myFile.println("Altitudine,Presiune,Temperatura,PPM");
-    myFile.close();
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
   }
-  else{
-    Serial.println("Eroare la deschiderea fisierului date.csv");
+  Serial.println("card initialized.");
+  // sd end
+  // ms begin
+   if (MS5611.begin() == true)
+  {
+    Serial.println("MS5611 found.");
   }
+  else
+  {
+    Serial.println("MS5611 not found. halt.");
+    while (1)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+    }
+  }
+  int result = MS5611.read();
+  altitudine_sol = func(MS5611.getPressure());
+  Serial.println(altitudine_sol);
+  // ms end
+  Serial.println();
+  if(SD.exists("datalog.csv")) {
+    SD.remove("datalog.csv");
+  }
+  dataFile = SD.open("datalog.csv", FILE_WRITE);
 }
+
+void sensor_values()
+{
+  start = micros();
+  int result = MS5611.read();
+  stop = micros();
+  int analogSensor = analogRead(smokeA0);
+  if (result != MS5611_READ_OK)
+  {
+    Serial.print("Error in read: ");
+    Serial.println(result);
+  }
+  else
+  {
+    Serial.print("T:\t");
+    Serial.print(MS5611.getTemperature(), 2);
+    dataFile.print(MS5611.getTemperature(), 2);
+    dataFile.print(", ");
+    Serial.print("\tP:\t");
+    Serial.print(altitudine(MS5611.getPressure()));
+    dataFile.print(altitudine(MS5611.getPressure()));
+    dataFile.print(", ");
+    Serial.print("\tt:\t");
+    Serial.print(stop - start);
+    dataFile.print(stop - start);
+    Serial.print("S:\t");
+    Serial.print(analogSensor);
+    dataFile.print(", ");
+    dataFile.print(analogSensor);
+    dataFile.println();
+    Serial.println();
+  }
+  dataFile.flush();
+}
+
 
 void loop() {
-  double temperatura = ms5611.getTemperature();                                               // Masurata in grade celsius
-  double presiune = ms5611.getPressure();                                                     // Masurata in milibari 1 mBa = 100 Pa
-  double altitudine = 44330.0 * (1-pow(presiune/presiuneSol, 1/5.255));                       // Altitudinea rachetei
-  float ppm = mq135_sensor.getPPM();                                                          // Calitate aer
-
-  myFile = SD.open("date.csv", FILE_WRITE);
-  if(myFile){
-    myFile.print(altitudine);
-    myFile.print(",");
-    myFile.print(presiune);
-    myFile.print(",");
-    myFile.print(temperatura);
-    myFile.print(",");
-    myFile.println(ppm);
-    myFile.close();
-  }
-  else{
-    Serial.println("Eroare la deschiderea fisierului date.csv");
-  }
-  
-  //debug_ms5611(temperatura, presiune, altitudineAbsoluta, altitudine);
-  //delay(500);
-}
-
-void debug_ms5611(double t, double p, double a, double A){
-  Serial.println("-------------------");
-  Serial.print("Temperatura: ");
-  Serial.println(t);
-  Serial.print("Presiune: ");
-  Serial.println(p);
-  Serial.print("Altitudinea relativa marii: ");
-  Serial.println(a);
-  Serial.print("Altitudinea reala: ");
-  Serial.println(A);
-  Serial.println();
+  digitalWrite(LED_BUILTIN, HIGH);
+  MS5611.setOversampling(OSR_STANDARD);
+  sensor_values();
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
